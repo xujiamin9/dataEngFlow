e8a794300990aa253a6fd314259bee6ffb0d01c0
xujiamin9 <jiamingjxu@gmail.com>
2024-11-27 00:50:48 +0800

    can publish to kafka with random transactions

--- fakerKafkaPub.py
+++ fakerKafkaPub.py
@@ -3,11 +3,9 @@ import random
 from datetime import datetime, timedelta
 from kafka import KafkaProducer
 import json
-from bson import json_util
-
 
 fake = Faker()
-fake.seed_instance(0)
+fake.seed_instance(random.randint(0, 20))
 
 def generate_transaction():
     amount = round(random.uniform(10, 5000), 2)
@@ -24,23 +22,39 @@ def generate_transaction():
         "category": fake.word(ext_word_list=["food", "transport", "entertainment", "utilities", "shopping"])
     }
 
+def on_send_success(record_metadata):
+    print(record_metadata.topic)
+    print(record_metadata.partition)
+    print(record_metadata.offset)
+
+def on_send_error(excp):
+    log.error('I am an errback', exc_info=excp)
+
 # Generate 10 transactions
 transactions = [generate_transaction() for _ in range(10)]
 
 # Sort transactions by date
 transactions.sort(key=lambda x: x['date'])
 
-for transaction in transactions:
+#for transaction in transactions:
     # print(f"Date: {transaction['date']}")
     # print(f"Amount: {transaction['amount']}")
     # print(f"Description: {transaction['description']}")
     # print(f"Merchant: {transaction['merchant']}")
     # print(f"Category: {transaction['category']}")
-    print(transaction)
-    print('\n')
+    # print(transaction)
+    # print('\n')
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+
+)
 
 for transaction in transactions:
-    json_transaction = json.dumps(transaction,  default=json_util.default).encode('utf-8')
-    producer.send('transactions', json_transaction)
+    producer.send('transactions', json.dumps(transaction).encode('utf-8')).add_callback(on_send_success).add_errback(on_send_error)
+
+    # block until all async messages are sent
+    producer.flush()
+
+    # configure multiple retries
+    producer = KafkaProducer(retries=5)
